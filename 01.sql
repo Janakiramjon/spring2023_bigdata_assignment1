9f5bc77c82fa1c50c2cccdb647e2c97bf3d727a9
Niq Giordano <niqgiordano@gmail.com>
2023-02-08 19:38:36 -0500

    Completed Person 2 role for this Homework Assignment

--- 01.sql
+++ 01.sql
@@ -1,17 +1,10 @@
-create table if not exists git_assignment(
-
-id      int auto_increment primary key
-
-,accountID  int         NOT NULL
-
-,ownerName  varchar(255) NOT NULL
-
-,joinDate   date        NOT NULL
-
-,balance decimal(10,2)   NULL
-
-,active boolean     NOT NULL
-
-,comments   text        
-
+create table if not exists git_assignment (
+id 		INT 		NOT NULL	AUTO_INCREMENT	PRIMARY KEY,
+account_id  	INT         	NOT NULL,
+owner_name  	VARCHAR(255) 	NOT NULL,
+join_date   	DATE        	NOT NULL,
+balance 	DECIMAL(10,2),
+active 		BOOLEAN 	NOT NULL,
+comments 	TEXT,
+status 		CHAR(1)
 );
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # spring2023_bigdata_assignment1
 Team BG 3
 Person 1: Janakiram Jonnalagadda
+Person 2: Nicholas Giordano
+
+	Changes Made to 01.sql
+
+		1. Added the column "status" to the table.
+		2. Changed variable names to Snake Case to better align with SQL conventions.
+		3. Changed keywords to Uppercase to better align with SQL conventions.
+		4. Removed NULL after balance because of redundancy.
+		5. Changed spacing and alignment to improve readability.
+
+	Is it necessary to have "id" as the primary key when we also have "account_id"?
+
